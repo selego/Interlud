d81d926b43c09750fa6a7afd7698e63a29d3c2c0
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-10-10 16:18:54 +0200

    fixe model

--- api/src/models/action_indicator_value.js
+++ api/src/models/action_indicator_value.js
@@ -5,7 +5,13 @@ const MODELNAME = "action_indicator_value";
 const Schema = new mongoose.Schema(
   {
     action_id: { type: String, trim: true },
-    indicator_id_value: { type: String, trim: true },
+    collectivity_id: { type: String, trim: true },
+    indicator_id: { type: String, trim: true },
+    situation: { type: String, enum: ["init", "ref", "prev", "expost"], trim: true },
+    year: { type: Number, trim: true },
+    value: { type: Number, trim: true },
+    source: { type: String, trim: true },
+    comment: { type: String, trim: true },
   },
   { timestamps: true },
 );
