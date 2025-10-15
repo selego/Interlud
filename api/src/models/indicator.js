65577ec992827ade927d21a5897fee75cb738c37
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-10-15 18:12:33 +0200

    Update indicator.js

--- api/src/models/indicator.js
+++ api/src/models/indicator.js
@@ -6,9 +6,8 @@ const Schema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
     description: { type: String, trim: true },
-    unit: { type: String, trim: true },
+    value_unit: { type: String, trim: true },
     value_type: { type: String, trim: true },
-    unit: { type: String, trim: true },
     category: { type: String, trim: true },
     sub_category: { type: String, trim: true },
   },
