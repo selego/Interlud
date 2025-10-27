5fe611c61fbc6ffbace7790fa5c77d21b55dff40
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-10-27 18:07:14 +0100

    Clean debounce input

--- app/src/components/debounceInput.jsx
+++ app/src/components/debounceInput.jsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-const DebounceInput = ({ debounce = 400, isTextArea = false, ...props }) => {
-  const [input, setInput] = useState(props.value);
-
+const DebounceInput = ({debounce = 400,value = "",onChange, ...otherProps }) => {
+  const [inputValue, setInputValue] = useState(value);
+  
   useEffect(() => {
-    if (props.value === input) return;
-    const handler = setTimeout(() => {
-      props.onChange({ target: { value: input } });
+    if (value === inputValue) return;
+    const timeoutId = setTimeout(() => {
+      onChange?.({ target: { value: inputValue } });
     }, debounce);
-    return () => clearTimeout(handler);
-  }, [input]);
+    return () => clearTimeout(timeoutId);
+  }, [inputValue, value, onChange, debounce]);
 
-  return isTextArea ? (
-    <textarea {...props} value={input} onChange={(e) => setInput(e.target.value)} />
-  ) : (
-    <input {...props} value={input} onChange={(e) => setInput(e.target.value)} />
-  );
+  return <input {...otherProps} value={inputValue} onChange={(event) => setInputValue(event.target.value)} />;
 };
 
 export default DebounceInput;
