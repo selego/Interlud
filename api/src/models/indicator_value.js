f1f0f6ecaf8e673e21055dc4f03732deb319fafa
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-11-20 15:06:48 +0100

    update model

--- api/src/models/indicator_value.js
+++ api/src/models/indicator_value.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const MODELNAME = "indicator_value";
 
+const type = {
+  text: { type: String, trim: true },
+  number: { type: Number, trim: true },
+  radio: { type: Array, default: [] },
+  checkbox: { type: Array, default: [] },
+}
+
 const Schema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
