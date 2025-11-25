b3b4c70dfe73f6298a989545989416f5bb466640
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-11-25 15:22:36 +0100

    fixe

--- app/src/config.js
+++ app/src/config.js
@@ -3,7 +3,7 @@ const environment = getEnvironment()
 let apiURL = ""
 if (environment === "development") apiURL = "http://localhost:8080"
 if (environment === "production") {
-  apiURL = "https://interlud-api.cleverapps.io/"
+  apiURL = "https://interlud-api.cleverapps.io"
 }
 
 const SENTRY_URL = "YOUR_SENTRY_URL"
