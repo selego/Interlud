96f3c9421a113ada38dddd50e46992a4c5585976
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2025-11-27 11:59:31 +0100

    Update index.jsx

--- app/src/scenes/home/index.jsx
+++ app/src/scenes/home/index.jsx
@@ -499,7 +499,7 @@ export default function Home() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {actions.map((action) => (
-            <CardAction key={action._id} action={action} />
+            <CardAction action={action} />
           ))}
 
           <div
